504fe4146ba89bd84b9b2e92d924f2f678fa8bfe
lawrenceagles <eaglescoder@gmail.com>
2022-04-21 11:07:33 +0000

    working hello world boilerplate

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+// gets abosolute path of the folder.
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+	mode: 'development',
+	entry: './src/index.js',
+	output: {
+		path: path.resolve(__dirname + '/dist'),
+		filename: 'bundle.js'
+	},
+	watch: true,
+	plugins: [new HtmlWebpackPlugin()]
+};
